4d7a69604c98aee1d66a6b58be528743fbdc1bce
vinicio17 <164871221+vinicio17@users.noreply.github.com>
2024-11-11 07:55:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Handebol',
+    'Em que ano o handebol foi fundao',
+    '1919.'
+)
+
+criaCartao(
+    'nataçõ',
+    'Existe quantos tipos de natação?',
+    'Existe quatro tipo.'
+)
+
+criaCartao(
+    'Futsal',
+    'Quem foi o criador do Futsal?',
+    'Foi o Juan Carlos Ceriani.'
+)
+
+criaCartao(
+    'Beisebol',
+    'Como faz para marcar ponto no Beisebol?',
+    'Se a bola for acertada pelo rebatedor, ele deve correr pelas quatro bases do campo. Se ele conseguir atingir as quatro, a equipe ganha um ponto.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual país é considerado o país o Futebol?',
+    'O Brail.'
+)
+
+criaCartao(
+    'Olimpíadas',
+    'Em qual pais foi realizada as Olimpíadas 2024?',
+    'Paris.'
+)
+
+criaCartao(
+    'Handebol',
+    'Em quantos países o Handebol é praticado?',
+    'O Handebol é praticado em mais de 180 países do mundo.'
+)
+
+criaCartao(
+    'Basquete',
+    'Em qual time Michael Jordan passou a maior parte de sua carreira jogando?',
+    'Chicago Bulls.'
+)
+
+criaCartao(
+    'Rugby',
+    'Quantos jogadores formam uma equipe de rugby?',
+    'As equipes de rugby são compostas por 15 jogadores.'
+)
